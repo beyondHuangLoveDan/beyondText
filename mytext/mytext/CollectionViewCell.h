1b162ba3b7ba1c1bdf360975f9ed5941188c8901
黄进 <huangjin@huangjindemacbook-air.local>
2017-04-07 16:39:33 +0800

    haugjin 2.0

---
+++ mytext/mytext/CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.h
+//  mytext
+//
+//  Created by 黄进 on 17/4/7.
+//  Copyright © 2017年 黄进. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ mytext/mytext/CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.m
+//  mytext
+//
+//  Created by 黄进 on 17/4/7.
+//  Copyright © 2017年 黄进. All rights reserved.
+//
+
+#import "CollectionViewCell.h"
+
+@implementation CollectionViewCell
+
+@end
